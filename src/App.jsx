85523c2ae67617b01058e958d8820eda7de82722
김민지 <kimmj1@focusmediakorea.com>
2023-02-21 15:12:46 +0900

    initiate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container1">
+    <h1>단지 등록</h1>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
